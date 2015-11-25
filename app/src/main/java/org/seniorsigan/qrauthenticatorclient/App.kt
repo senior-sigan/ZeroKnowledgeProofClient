20224fde9ec903d45f0ebe8ab51985e39f539184
Ilya Siganov <ilya.blan4@gmail.com>
2015-11-25 22:55:11 +0600

    init android app with kotlin

---
+++ app/src/main/java/org/seniorsigan/qrauthenticatorclient/App.kt
@@ -0,0 +1,12 @@
+package org.seniorsigan.qrauthenticatorclient
+
+import android.app.Application
+import android.content.Context
+import android.support.multidex.MultiDex
+
+class App: Application() {
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+}
